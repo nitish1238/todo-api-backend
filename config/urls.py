f94a113be874f0cbc9f136b5514a95681fc31808
nitish1238 <nitishkumaryadav2205@gmail.com>
2026-02-12 15:45:23 +0530

    add home route

--- config/urls.py
+++ config/urls.py
@@ -15,19 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,)
+    TokenRefreshView,
+)
 
+# Home page
+def home(request):
+    return HttpResponse("API Running Successfully")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
-
-
-
-
+    path("", home),   # 👈 ADD THIS LINE
+    path("admin/", admin.site.urls),
+    path("api/", include("tasks.urls")),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
+
